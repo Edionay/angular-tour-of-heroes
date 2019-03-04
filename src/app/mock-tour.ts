79309c51a4cc39204dad3198a02d8625f5c85eb1
Edionay Aguiar <edionay@gmail.com>
2019-03-04 00:03:27 -0300

    Home page update.

--- src/app/mock-tour.ts
+++ src/app/mock-tour.ts
@@ -4,11 +4,8 @@ export const TOURLIST: Tour[] = [
   { name: 'UnB' },
   { name: 'Concreta' },
   { name: 'Catedral Metropolitana de Brasília' },
-  { name: 'Celeritas' },
-  { name: 'Magneta' },
-  { name: 'RubberMan' },
-  { name: 'Dynama' },
-  { name: 'Dr IQ' },
-  { name: 'Magma' },
-  { name: 'Tornado' }
+  { name: 'Cachorro-quente' },
+  { name: 'Memorial JK' },
+  { name: 'Museu Nacional de Brasília' },
+
 ]
