ec55f34b28a380bb6cca5c875af9ababee47eb27
Edionay Aguiar <edionay@gmail.com>
2019-03-04 01:31:56 -0300

    Materialize-css.

--- src/app/tour.service.ts
+++ src/app/tour.service.ts
@@ -1,15 +1,18 @@
 import { Injectable } from '@angular/core';
 import { Tour } from './tour'
 import { TOURLIST } from './mock-tour'
+import { Observable, of } from 'rxjs'
+import { MessageService } from './message.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class TourService {
 
-  constructor() { }
+  constructor(private messageService: MessageService) { }
 
-  getHeroes(): Tour[] {
-    return TOURLIST;
+  getTourList(): Observable<Tour[]> {
+    this.messageService.add('HeroService: fetched heroes');
+    return of(TOURLIST)
   }
 }
--- src/app/tour/tour.component.ts
+++ src/app/tour/tour.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Tour } from '../tour'
-import { TOURLIST } from '../mock-tour'
+import { TourService } from '../tour.service'
 
 @Component({
   selector: 'app-tour',
@@ -9,16 +9,24 @@ import { TOURLIST } from '../mock-tour'
 })
 export class TourComponent implements OnInit {
 
-  tourList = TOURLIST
+  tourList: Tour[]
   selectedTour: Tour
 
   onSelect(tour: Tour): void {
     this.selectedTour = tour
   }
 
-  constructor() { }
+  constructor(private tourService: TourService) {
+
+   }
 
   ngOnInit() {
+    this.getHeroes()
   }
 
+  getHeroes(): void {
+    this.tourService.getTourList().subscribe(tourList => {
+      this.tourList = tourList
+    })
+  }
 }
